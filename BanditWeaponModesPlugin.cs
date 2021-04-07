bef65ecaa3f90f2e2dfdd6e994dee7966adac9b6
Pierre Ziegler <vl4dimyr@gmail.com>
2021-04-07 22:45:29 +0200

    Added: files

---
+++ BanditWeaponModesPlugin.cs
@@ -0,0 +1,341 @@
+﻿using System;
+using BepInEx;
+using BepInEx.Configuration;
+using UnityEngine;
+using R2API.Utils;
+using RoR2.UI;
+using RoR2;
+using EntityStates.Bandit2.Weapon;
+
+namespace BanditWeaponModes
+{
+    enum FireMode { Normal, Spam, DoubleDoubleTap, Count }
+
+    [BepInDependency("com.bepis.r2api", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInPlugin("de.userstorm.banditweaponmodes", "BanditWeaponModes", "{VERSION}")]
+    [NetworkCompatibility(CompatibilityLevel.NoNeedForSync, VersionStrictness.DifferentModVersionsAreOk)]
+    public class BanditWeaponModesPlugin : BaseUnityPlugin
+    {
+        public static ConfigEntry<string> DefaultMode { get; set; }
+        public static ConfigEntry<bool> EnableModeSelectionWithNumberKeys { get; set; }
+        public static ConfigEntry<bool> EnableModeSelectionWithMouseWheel { get; set; }
+        public static ConfigEntry<bool> EnableModeSelectionWithDPad { get; set; }
+        public static ConfigEntry<int> DelayBetweenDoubleDoubleTaps { get; set; }
+        public static ConfigEntry<int> DelayBetweenShots { get; set; }
+
+        private FireMode fireMode = FireMode.Normal;
+        private float fixedAge = 0;
+        private bool fire = false;
+        private bool reloadLock = false;
+        private bool hasLeftLandingPod = false;
+        private HGTextMeshProUGUI modeLabel = null;
+
+        private void CycleFireMode (bool forward = true)
+        {
+            FireMode newFireMode = fireMode + (forward ? 1 : -1);
+
+            if (newFireMode == FireMode.Count)
+            {
+                newFireMode = FireMode.Normal;
+            }
+
+            if ((int)newFireMode == -1)
+            {
+                newFireMode = FireMode.Count - 1;
+            }
+
+            fireMode = newFireMode;
+        }
+
+        private void InitConfig()
+        {
+            DefaultMode = Config.Bind<string>(
+                "Settings",
+                "DefaultMode",
+                "Normal",
+                "The mode that is selected on game start. Modes: Normal, Spam, DoubleDoubleTap"
+            );
+
+            EnableModeSelectionWithNumberKeys = Config.Bind<bool>(
+               "Settings",
+               "EnableModeSelectionWithNumberKeys",
+               true,
+               "When set to true modes can be selected using the number keys"
+            );
+
+            EnableModeSelectionWithMouseWheel = Config.Bind<bool>(
+               "Settings",
+               "EnableModeSelectionWithMouseWheel",
+               true,
+               "When set to true modes can be cycled through using the mouse wheel"
+            );
+
+            EnableModeSelectionWithDPad = Config.Bind<bool>(
+               "Settings",
+               "EnableModeSelectionWithDPad",
+               true,
+               "When set to true modes can be cycled through using the DPad (controller)"
+            );
+
+            DelayBetweenDoubleDoubleTaps = Config.Bind<int>(
+               "Settings",
+               "DelayBetweenDoubleDoubleTaps",
+               400,
+               "The delay (in milliseconds) between the 2nd and 3rd shot when using the DoubleDoubleTap Mode"
+            );
+
+            DelayBetweenShots = Config.Bind<int>(
+               "Settings",
+               "DelayBetweenShots",
+               125,
+               "The delay (in milliseconds) between the shots"
+            );
+        }
+
+        private void HandleConfig()
+        {
+            try
+            {
+                fireMode = (FireMode)Enum.Parse(typeof(FireMode), DefaultMode.Value);
+            }
+            catch (Exception)
+            {
+                fireMode = FireMode.Normal;
+            }
+        }
+
+        private HGTextMeshProUGUI CreateLabel (Transform parent, String name, String text, Vector2 position)
+        {
+            GameObject textContainer = new GameObject(name);
+
+            textContainer.transform.parent = parent;
+
+            textContainer.AddComponent<CanvasRenderer>();
+
+            RectTransform rectTransform = textContainer.AddComponent<RectTransform>();
+            HGTextMeshProUGUI hgtextMeshProUGUI = textContainer.AddComponent<HGTextMeshProUGUI>();
+
+            hgtextMeshProUGUI.text = text;
+            hgtextMeshProUGUI.fontSize = 12f;
+            hgtextMeshProUGUI.color = Color.white;
+            hgtextMeshProUGUI.alignment = TMPro.TextAlignmentOptions.Center;
+            hgtextMeshProUGUI.enableWordWrapping = false;
+
+            rectTransform.localPosition = Vector2.zero;
+            rectTransform.anchorMin = Vector2.zero;
+            rectTransform.anchorMax = Vector2.one;
+            rectTransform.localScale = Vector3.one;
+            rectTransform.sizeDelta = Vector2.zero;
+            rectTransform.anchoredPosition = position;
+
+            return hgtextMeshProUGUI;
+        }
+
+        private void SelectFireModeWithNumberKeys()
+        {
+            if (!EnableModeSelectionWithNumberKeys.Value)
+            {
+                return;
+            }
+
+            if (Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                fireMode = FireMode.Normal;
+
+                return;
+            }
+
+            if (Input.GetKeyDown(KeyCode.Alpha2))
+            {
+                fireMode = FireMode.Spam;
+
+                return;
+            }
+
+            if (Input.GetKeyDown(KeyCode.Alpha3))
+            {
+                fireMode = FireMode.DoubleDoubleTap;
+            }
+        }
+
+        private void SelectFireModeWithMouseWheel()
+        {
+            if (!EnableModeSelectionWithMouseWheel.Value)
+            {
+                return;
+            }
+
+            float wheel = Input.GetAxis("Mouse ScrollWheel");
+
+            if (wheel == 0)
+            {
+                return;
+            }
+
+            // scroll down => forward; scroll up => backward
+            CycleFireMode(wheel < 0f);
+        }
+
+        private void SelectFireModeWithDPad()
+        {
+            if (!EnableModeSelectionWithDPad.Value)
+            {
+                return;
+            }
+
+            if (DPad.GetInputDown(DPadInput.Right) || DPad.GetInputDown(DPadInput.Down))
+            {
+                CycleFireMode();
+
+                return;
+            }
+
+            if (DPad.GetInputDown(DPadInput.Left) || DPad.GetInputDown(DPadInput.Up))
+            {
+                CycleFireMode(false);
+            }
+         }
+
+        private void SelectFireMode()
+        {
+            SelectFireModeWithNumberKeys();
+            SelectFireModeWithMouseWheel();
+            SelectFireModeWithDPad();
+        }
+
+        private float getDelay(int charges)
+        {
+            if (fireMode == FireMode.DoubleDoubleTap && charges % 2 == 0)
+            {
+                return (float) DelayBetweenDoubleDoubleTaps.Value / 1000f;
+            }
+
+            return (float) DelayBetweenShots.Value / 1000f;
+        }
+
+        private void FireLogic(EntityStateMachine self)
+        {
+            var skill = self.commonComponents.skillLocator.primary;
+            var inputBank = self.commonComponents.inputBank;
+            bool fire = inputBank && inputBank.skill1.down;
+            bool justPressed = inputBank && inputBank.skill1.justPressed;
+            bool isReloading = self.state.ToString() == "EntityStates.Bandit2.Weapon.Reload";
+            int charges = skill.stock;
+            float delay = getDelay(charges);
+
+            if (charges == 0 || isReloading)
+            {
+                reloadLock = true;
+            }
+
+            if ((charges == skill.maxStock && !isReloading) || justPressed)
+            {
+                reloadLock = false;
+            }
+
+            if (!reloadLock && charges > 0 && fire && fixedAge > delay)
+            {
+                fixedAge = 0;
+
+                // ignore first shot since it's handled by the game
+                if (justPressed)
+                {
+                    return;
+                }
+
+                if (skill.skillNameToken == "BANDIT2_PRIMARY_NAME")
+                {
+                    self.SetNextState(new FireShotgun2());
+                }
+                else
+                {
+                    self.SetNextState(new Bandit2FireRifle());
+                }
+
+                skill.DeductStock(1);
+            }
+        }
+
+        public void FixedUpdateHook(On.RoR2.EntityStateMachine.orig_FixedUpdate orig, EntityStateMachine self)
+        {
+            orig.Invoke(self);
+
+            var body = self.commonComponents.characterBody;
+            bool isBandit = body && body.baseNameToken == "BANDIT2_BODY_NAME";
+
+            if (!isBandit)
+            {
+                return;
+            }
+
+            // if no longer EntityStates.SpawnTeleporterState or EntityStates.GenericCharacterPod
+            if (
+                !hasLeftLandingPod &&
+                self.customName == "Body" &&
+                self.state.ToString() == "EntityStates.GenericCharacterMain"
+            )
+            {
+                hasLeftLandingPod = true;
+
+                return;
+            }
+
+            if (hasLeftLandingPod && self.customName == "Weapon" && fireMode != FireMode.Normal)
+            {
+                fixedAge += Time.fixedDeltaTime;
+
+                FireLogic(self);
+            }
+        }
+
+        public void UpdateHook(On.RoR2.UI.SkillIcon.orig_Update orig, SkillIcon self)
+        {
+            orig.Invoke(self);
+
+            if (self.targetSkill && self.targetSkillSlot == SkillSlot.Primary)
+            {
+                if (self.targetSkill.characterBody.baseNameToken == "BANDIT2_BODY_NAME")
+                {
+                    if (!modeLabel) {
+                        var pos = self.stockText.transform.position;
+
+                        modeLabel = CreateLabel(
+                            self.stockText.transform.parent,
+                            "ModeLabel",
+                            fireMode.ToString(),
+                            new Vector2(pos.x - 7.5f, pos.y + 20f)
+                        );
+
+                        modeLabel.transform.rotation = self.stockText.transform.rotation;
+                        modeLabel.color = self.stockText.color;
+                        modeLabel.alignment = self.stockText.alignment;
+                    }
+
+                    modeLabel.SetText(fireMode.ToString());
+                }
+            }
+        }
+
+        public void Awake()
+        {
+            InitConfig();
+            HandleConfig();
+
+            On.RoR2.EntityStateMachine.FixedUpdate += FixedUpdateHook;
+            On.RoR2.UI.SkillIcon.Update += UpdateHook;
+        }
+
+        public void Update()
+        {
+            DPad.Update();
+
+            SelectFireMode();
+        }
+
+        public void OnDestroy()
+        {
+            On.RoR2.EntityStateMachine.FixedUpdate -= FixedUpdateHook;
+            On.RoR2.UI.SkillIcon.Update -= UpdateHook;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,92 @@
+# BanditWeaponModes
+
+## Description
+
+This mod allows you to choose between 3 firing modes for the bandit's primary weapon: `Normal`, `Spam` and `DoubleDoubleTap`.
+With `Spam` and `DoubleDoubleTap` mode you can just hold down the fire key and don´t have to spam it.
+
+The modes can be selected using the number keys `1`, `2` and `3`, or cycled through using the `mouse wheel` or [DPad](https://en.wikipedia.org/wiki/D-pad).
+The current mode is displayed above the primary skill icon.
+
+## Modes
+
+| Mode            | Key | Description | Screenshot |
+|-----------------|-----|-------------|------------|
+| Normal          |  1  | The default mode/behavior of the bandit's primary weapon. | ![normal](https://raw.githubusercontent.com/Vl4dimyr/BanditWeaponModes/master/images/sc_normal.jpg)
+| Spam            |  2  | Automatically fires all shots as long as the fire key is pressed. | ![spam](https://raw.githubusercontent.com/Vl4dimyr/BanditWeaponModes/master/images/sc_spam.jpg)
+| DoubleDoubleTap |  3  | Automatically double taps twice as long as the fire key is pressed. | ![double_double_tap](https://raw.githubusercontent.com/Vl4dimyr/BanditWeaponModes/master/images/sc_double_double_tap.jpg)
+
+### Cycle through modes
+
+| Direction | Actions                                 |
+|-----------|-----------------------------------------|
+| Forward   | Mouse Wheel Down, DPad Right, DPad Down |
+| Backward  | Mouse Wheel Up, DPad Left, DPad Up      |
+
+> Currently this is only tested with a xbox 360 controller (xbox one should work too).
+
+## Config
+
+The config file (`\BepInEx\config\de.userstorm.banditweaponmodes.cfg`) will be crated automatically when the mod is loaded.
+You need to restart the game for changes to apply in game.
+
+### Example config
+
+The example config keeps only mode selection with number keys enabled and sets the default mode to `DoubleDoubleTap`.
+
+```ini
+## Settings file was created by plugin BanditWeaponModes v1.0.0
+## Plugin GUID: de.userstorm.banditweaponmodes
+
+[Settings]
+
+## The mode that is selected on game start. Modes: Normal, Spam, DoubleDoubleTap
+# Setting type: String
+# Default value: Normal
+DefaultMode = DoubleDoubleTap
+
+## When set to true modes can be selected using the number keys
+# Setting type: Boolean
+# Default value: true
+EnableModeSelectionWithNumberKeys = true
+
+## When set to true modes can be cycled through using the mouse wheel
+# Setting type: Boolean
+# Default value: true
+EnableModeSelectionWithMouseWheel = false
+
+## When set to true modes can be cycled through using the DPad (controller)
+# Setting type: Boolean
+# Default value: true
+EnableModeSelectionWithDPad = false
+
+## The delay (in milliseconds) between the 2nd and 3rd shot when using the DoubleDoubleTap Mode
+# Setting type: Int32
+# Default value: 400
+DelayBetweenDoubleDoubleTaps = 400
+
+## The delay (in milliseconds) between the shots
+# Setting type: Int32
+# Default value: 125
+DelayBetweenShots = 125
+```
+
+## Manual Install
+
+- Install [BepInEx](https://thunderstore.io/package/bbepis/BepInExPack/) and [R2API](https://thunderstore.io/package/tristanmcpherson/R2API/)
+- Download the latest `BanditWeaponModes_x.y.z.zip` [here](https://thunderstore.io/package/Vl4dimyr/BanditWeaponModes/)
+- Extract and move the `BanditWeaponModes.dll` into the `\BepInEx\plugins` folder
+
+## Changelog
+
+The [Changelog](https://github.com/Vl4dimyr/BanditWeaponModes/blob/master/CHANGELOG.md) can be found on GitHub.
+
+## Bugs/Feedback
+
+For bugs or feedback please use [GitHub Issues](https://github.com/Vl4dimyr/BanditWeaponModes/issues).
+
+## Help me out
+
+[![Patreon](https://cdn.iconscout.com/icon/free/png-64/patreon-2752105-2284922.png)](https://www.patreon.com/vl4dimyr)
+
+It is by no means required, but if you would like to support me head over to [my Patreon](https://www.patreon.com/vl4dimyr).
